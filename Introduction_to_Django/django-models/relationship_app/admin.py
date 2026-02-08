128075e6afa19f9beaed42c1b96cb13511da767e
FredrickMbithi <fredrickmbithi000@gmail.com>
2026-02-08 12:00:06 +0300

    Implement models and admin registration for relationship_app, add sample queries

--- Introduction_to_Django/django-models/relationship_app/admin.py
+++ Introduction_to_Django/django-models/relationship_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Book, Library, Librarian
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'author')
+
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+	filter_horizontal = ('books',)
+
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'library')
