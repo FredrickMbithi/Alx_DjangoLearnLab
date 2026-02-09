288adb9fe6cf8da45ed377c53fed5365cc238e31
FredrickMbithi <fredrickmbithi000@gmail.com>
2026-02-10 01:22:03 +0300

    gylk

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,32 @@
-"""Compatibility wrapper re-exporting query helper functions.
-
-This file forwards calls to the real `relationship_app.query_samples` so code
-importing `LibraryProject.relationship_app.query_samples` will work as expected.
 """
-from relationship_app.query_samples import (
-    get_books_by_author_name,
-    get_books_in_library,
-    get_librarian_for_library,
-)
+Sample queries for `relationship_app` demonstrating ORM relationships.
 
-__all__ = [
-    'get_books_by_author_name',
-    'get_books_in_library',
-    'get_librarian_for_library',
-]
+Use these helpers inside `manage.py shell` or any Django context.
+"""
+from typing import Iterable, Optional
+
+from .models import Author, Book, Library, Librarian
+
+
+def get_books_by_author_name(author_name: str) -> Iterable[Book]:
+    try:
+        author = Author.objects.get(name=author_name)
+    except Author.DoesNotExist:
+        return Book.objects.none()
+    return Book.objects.filter(author=author)
+
+
+def get_books_in_library(library_name: str) -> Iterable[Book]:
+    try:
+        library = Library.objects.get(name=library_name)
+    except Library.DoesNotExist:
+        return Book.objects.none()
+    return library.books.all()
+
+
+def get_librarian_for_library(library_name: str) -> Optional[Librarian]:
+    try:
+        library = Library.objects.get(name=library_name)
+    except Library.DoesNotExist:
+        return None
+    return getattr(library, 'librarian', None)
