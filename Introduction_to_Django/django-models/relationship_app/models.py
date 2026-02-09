39a96faca6c452576258975385e83ebe6ecee433
FredrickMbithi <fredrickmbithi000@gmail.com>
2026-02-10 00:27:39 +0300

    Add publication_year field to Book model with default value of 2023

--- Introduction_to_Django/django-models/relationship_app/models.py
+++ Introduction_to_Django/django-models/relationship_app/models.py
@@ -11,7 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
 	title = models.CharField(max_length=255)
 	author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-	publication_year = models.IntegerField()
+	publication_year = models.IntegerField(default=2023)
 
 	def __str__(self):
 		return self.title
