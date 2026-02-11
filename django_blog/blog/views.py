8088317f2b0bcca3d302b5a8e156e2b0879fff87
FredrickMbithi <fredrickmbithi000@gmail.com>
2026-02-12 00:43:01 +0300

    Implement blog functionality with post listing and template rendering; update URLs and admin registration

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    # Fetch all posts from the database
+    posts = Post.objects.all().order_by('-published_date')
+
+    # Send data to template
+    return render(request, 'blog/index.html', {'posts': posts})
+
